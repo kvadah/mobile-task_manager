28531bc0b46bba381a5a2d411752084d0fc190d2
Khalid <kvadah9@gmail.com>
2025-06-05 15:08:57 +0300

    create database helper to deal with CRUD

---
+++ lib/database_helper.dart
@@ -0,0 +1,72 @@
+import 'package:sqflite/sqflite.dart';
+
+class DBHelper {
+  static final DBHelper _instance = DBHelper._internal();
+  factory DBHelper() => _instance;
+  DBHelper._internal();
+
+  static Database? _db;
+
+  Future<Database> get db async {
+    if (_db != null) return _db!;
+    _db = await initDb();
+    return _db!;
+  }
+
+  Future<Database> initDb() async {
+    final dbPath = await getDatabasesPath();
+    final path = join(dbPath, 'task_manager.db');
+
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: (db, version) async {
+        await db.execute('''
+          CREATE TABLE tasks(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title TEXT,
+            isCompleted INTEGER
+          )
+        ''');
+      },
+    );
+  }
+
+
+  // insert user new task
+  Future<int> insertTask(String title) async {
+    final dbClient = await db;
+    return await dbClient.insert(
+      'tasks',
+      {'title': title, 'isCompleted': 0},
+    );
+  }
+
+
+  //get all tasks to display
+  Future<List<Map<String, dynamic>>> getTasks() async {
+    final dbClient = await db;
+    return await dbClient.query('tasks');
+  }
+
+ // update a task
+    
+  Future<int> updateTask(int id, int isCompleted) async {
+    final dbClient = await db;
+    return await dbClient.update(
+      'tasks',
+      {'isCompleted': isCompleted},
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+  }
+//delete a tesk
+  Future<int> deleteTask(int id) async {
+    final dbClient = await db;
+    return await dbClient.delete(
+      'tasks',
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+  }
+}
