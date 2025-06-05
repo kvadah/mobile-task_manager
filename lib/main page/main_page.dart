da1e996d4c9d75cbb4fe74525d1c39af51461995
khalliid9 <kvadah9@gmail.com>
2025-06-05 13:53:43 +0300

    initial commit

---
+++ lib/main page/main_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TaskManager extends StatefulWidget {
+  const TaskManager({super.key});
+
+  @override
+  State<TaskManager> createState() => _TaskManagerState();
+}
+
+class _TaskManagerState extends State<TaskManager> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
